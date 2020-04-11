5daccf561e4314bd36c01c4121280f859716e0ff
Michelle Wingter <mwingter@gmail.com>
2020-04-11 18:29:34 -0300

    Iniciando T1 de redes
    Módulo 1 - Implementação de Sockets

---
+++ t1.c
@@ -0,0 +1,10 @@
+/* TRABALHO 1 - REDES
+
+	Nome: Michelle Wingter da Silva
+	nUSP: 10783243
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "t1.h"
---
+++ t1.h
@@ -0,0 +1,11 @@
+/* TRABALHO 1 - REDES
+
+	Nome: Michelle Wingter da Silva
+	nUSP: 10783243
+*/
+
+#ifndef T1_H
+
+
+
+#endif
